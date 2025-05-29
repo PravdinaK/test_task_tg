def784a628e7dfe1f1323b23bf8b749f22e03f89
Unique-Kris <titina91@mail.ru>
2025-05-29 15:03:20 +0300

    Added User entity and created interface UserRepository

---
+++ src/main/java/test_task/model/User.java
@@ -0,0 +1,28 @@
+package test_task.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String username;
+
+    private String languageCode;
+    private String photoUrl;
+    private Boolean allowsWriteToPm;
+
+}
