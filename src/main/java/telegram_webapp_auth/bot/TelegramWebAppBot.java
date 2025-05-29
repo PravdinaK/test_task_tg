e0bfbad8fda47aaa2e2e9b2b126117fc823839f4
Unique-Kris <titina91@mail.ru>
2025-05-30 00:03:07 +0300

    fix

--- src/main/java/telegram_webapp_auth/bot/TelegramWebAppBot.java
+++ src/main/java/telegram_webapp_auth/bot/TelegramWebAppBot.java
@@ -1,6 +1,5 @@
 package telegram_webapp_auth.bot;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
